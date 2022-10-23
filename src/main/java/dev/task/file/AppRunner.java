a2abebf734df6d6aea9e1b803c6cf990bd02b1de
alexanastasyev <alex.anastasyev@mail.ru>
2022-10-23 11:06:29 +0300

    Making SymbolInFilesSearcher work with files collection

--- src/main/java/dev/task/file/AppRunner.java
+++ src/main/java/dev/task/file/AppRunner.java
@@ -5,8 +5,11 @@ import java.util.function.Consumer;
 public class AppRunner {
     public static void main(String[] args) {
         Consumer<Boolean> onFinish = System.out::println;
-        SymbolsInTwoFilesSearcher searcher = new SymbolsInTwoFilesSearcher(onFinish);
-        searcher.search('a');
+        SymbolInFilesSearcher searcher = new SymbolInFilesSearcher(onFinish);
+        searcher.addFile("file1.txt");
+        searcher.addFile("file2.txt");
+        searcher.addFile("file3.txt");
+        searcher.search('a', 10);
     }
 
 }
---
+++ src/main/java/dev/task/file/SymbolInFilesSearcher.java
@@ -0,0 +1,63 @@
+package dev.task.file;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+
+public class SymbolInFilesSearcher {
+
+    private final Consumer<Boolean> onFinish;
+    private final Collection<String> filenames;
+    private ExecutorService executorService;
+
+    private int symbolsCounter = 0;
+    private boolean found = false;
+
+    public SymbolInFilesSearcher(Consumer<Boolean> onFinish) {
+        this.onFinish = onFinish;
+        this.filenames = new HashSet<>();
+    }
+
+    public void addFile(String filename) {
+        this.filenames.add(filename);
+    }
+
+    public void search(char searchSymbol, int amount) {
+        this.executorService = Executors.newFixedThreadPool(this.filenames.size());
+
+        Consumer<Character> onFind = (Character symbol) -> {
+            System.out.println(symbol);
+            incrementUntil(amount);
+        };
+
+        Collection<Future<Boolean>> futures = new ArrayList<>();
+        filenames.forEach(filename -> {
+            SearchSymbolInFileTask searchTask = new SearchSymbolInFileTask(filename, searchSymbol, onFind);
+            futures.add(executorService.submit(searchTask));
+        });
+
+        futures.forEach(future -> {
+            try {
+                future.get(10, TimeUnit.MINUTES);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+        executorService.shutdownNow();
+        if (!found) {
+            onFinish.accept(false);
+        }
+    }
+
+    private synchronized void incrementUntil(int amount) {
+        symbolsCounter++;
+        if (symbolsCounter == amount) {
+            found = true;
+            onFinish.accept(true);
+            executorService.shutdownNow();
+        }
+    }
+
+}
